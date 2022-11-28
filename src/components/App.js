cad93c63fee4f07faba40af7f423f028becc8488
Ursula Borrego <ursulaborrego@hotmail.com>
2022-11-28 10:09:31 +0000

    Render html

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,30 @@
 import '../styles/App.scss';
 import contacts from '../data/contacts.json';
+import {useState} from 'react';
 
 function App() {
+  
   // VARIABLES ESTADO
-
-  // USEEFFECT ?
-
+  const [data, setData] = useState(contacts.results);
+  
   // FUNCIONES HANDLER
 
   // FUNCIONES Y VARIABLES QUE AYUDEN A RENDERIZAR HTML
+  console.log(data);
+  const htmlData = data.map((oneContact) => {
+    return (
+    <tr key={oneContact.id}>
+      <td>{oneContact.name}</td>
+      <td>{oneContact.counselor}</td>
+      <td>{oneContact.speciality}</td>
+    </tr>
+    );
+  });
 
   // HTML EN EL RETURN
-
   return (
     <div className="App">
+      
       <h1 className="title">Adalabers</h1>
 
       <table className="table">
@@ -24,26 +35,9 @@ function App() {
           <th>Especialidad</th>
         </tr></thead>
         {/* <!-- Fin fila de cabecera --> */}
-        <tbody>
-        {/* <!-- Primera fila --> */}
-          <tr>
-            <td>MariCarmen</td>
-            <td>Yanelis</td>
-            <td>Python</td>
-          </tr>
-        {/* <!-- Segunda fila --> */}
-          <tr>
-            <td>Amparo</td>
-            <td>Dayana</td>
-            <td>IA</td>
-          </tr>
-        {/* <!-- Tercera fila --> */}
-          <tr>
-            <td>Escandia</td>
-            <td>Iván</td>
-            <td>3D graphics</td>
-          </tr>
-        </tbody>
+
+        <tbody>{htmlData}</tbody>
+
       </table>
     </div>
   );
